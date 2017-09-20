a2f7b48f9324e1e1adcda4ec601585d3a0ea588f
Michael Schmid <michael@amazee.com>
2017-09-20 08:32:11 +0200

    log req

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ var initDb = function(callback) {
 };
 
 app.get('/', function (req, res) {
+  console.log(req);
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
